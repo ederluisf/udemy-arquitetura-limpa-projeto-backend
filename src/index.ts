8e3cfc8f950068bef6ce3f9ceef499729287d396
Sgt  EDER <ederelfs@SGT-EDER.ccasj.intraer>
2024-02-26 14:12:20 -0300

    Configurando o Express pra startar na porta 4000

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
+import express from 'express';
+
+const app = express();
+const porta = process.env.API_PORTA ?? 4000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(porta, () => {
+  console.log(`🔥 Servidor executando na porta ${ porta }!!! 🔥`);  
+});
