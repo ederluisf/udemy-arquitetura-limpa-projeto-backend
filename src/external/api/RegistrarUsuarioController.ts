88391bc4588c04ad2d9d549a17f9a8b11c0d150e
Sgt  EDER <ederelfs@SGT-EDER.ccasj.intraer>
2024-02-26 16:58:47 -0300

    Implementação da rota de registrar usuários

---
+++ src/external/api/RegistrarUsuarioController.ts
@@ -0,0 +1,25 @@
+import RegistrarUsusario from '@/core/usuario/service/RegistrarUsuario';
+import { Express } from 'express';
+
+export default class RegistrarUsuarioController {
+
+  constructor(
+    servidor: Express,
+    casoDeUso: RegistrarUsusario
+  ) {
+    servidor.post('/api/usuarios/registrar', async(req, resp) => {
+      
+      try {
+        await casoDeUso.executar({
+          nome: req.body.nome,
+          email: req.body.email,
+          senha: req.body.senha
+        });
+        resp.status(201).send();
+      }
+      catch (erro: any) {
+        resp.status(400).send(erro.message);
+      }
+    });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import RepositorioUsuarioPostgre from './external/db/RepositorioUsuarioPostgre';
+import SenhaCripto from './external/auth/SenhaCripto';
+import RegistrarUsusario from './core/usuario/service/RegistrarUsuario';
+import RegistrarUsuarioController from './external/api/RegistrarUsuarioController';
 
 const app = express();
 const porta = process.env.API_PORTA ?? 4000;
@@ -12,3 +16,12 @@ app.use(express.urlencoded({ extended: true }));
 app.listen(porta, () => {
   console.log(`🔥 Servidor executando na porta ${ porta }!!! 🔥`);  
 });
+
+// ------------------------------------------- Rotas Abertas
+
+const repositorioUsuario = new RepositorioUsuarioPostgre();
+const provedorCripto = new SenhaCripto();
+
+const registrarUsuario = new RegistrarUsusario(repositorioUsuario, provedorCripto);
+
+new RegistrarUsuarioController(app, registrarUsuario);
